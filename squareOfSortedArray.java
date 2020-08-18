211f5ef333f7bb10d10882ea894c585136b6db8f
Rohit Verma <49588278+Rohitv0991@users.noreply.github.com>
2020-08-18 19:16:29 +0530

    Create squareOfSortedArray.java

---
+++ squareOfSortedArray.java
@@ -0,0 +1,18 @@
+/*Given an array of integers A sorted in non-decreasing order, 
+return an array of the squares of each number, also in sorted non-decreasing order.
+
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]*/
+
+//https://leetcode.com/problems/squares-of-a-sorted-array/
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        for(int i = 0; i < A.length; i++) {
+            A[i] = A[i]*A[i];
+        }
+        Arrays.sort(A);
+        return A;
+    }
+}
